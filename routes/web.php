8ee132793a712702d72e657990f225f4e3d080aa
ArjhayCOder <ralphjoshuasulit@gmai.com>
2023-09-25 08:38:32 +0800

    updated

--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,9 @@
 
 use App\Http\Controllers\Authentication\AuthController;
 use Illuminate\Support\Facades\Route;
+
+
+Route::controller(AuthController::class)->group(function () {
+    Route::any('/', 'login')->name('login')->middleware('guest');
+    Route::any('/', 'logout')->name('logout')->middleware('auth');
+});
