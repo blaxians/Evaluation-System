721b88ce13f441f59e70dc29e84c1f2e6c3d08b4
ArjhayCOder <ralphjoshuasulit@gmai.com>
2023-10-12 15:43:23 +0800

    Owa Works

--- app/Http/Controllers/Authentication/AuthController.php
+++ app/Http/Controllers/Authentication/AuthController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 
@@ -16,48 +17,69 @@ class AuthController extends Controller
         if ($request->isMethod('get')) {
             return view('pages.login');
         }
-        
+
         $validator = Validator::make($request->all(), [
             'username' => 'required',
             'password' => 'required'
         ]);
-    
+
         if ($validator->fails()) {
             $usernameError = $validator->errors()->first('username');
             $passwordError = $validator->errors()->first('password');
-        
+
             if ($usernameError && $passwordError) {
                 $message = 'Both fields are required.';
-            }
-            else if ($usernameError) {
+            } else if ($usernameError) {
                 $message = $usernameError;
             } else if ($passwordError) {
                 $message = $passwordError;
             }
-        
+
             return view('pages.login')->with('message', $message);
         }
-        
+
         $credentials = [
             'username' => $request->input('username'),
             'password' => $request->input('password')
         ];
-    
+
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
             return redirect()->route('loading');
         } else {
             $message = 'Invalid username or password';
-        
+
             if (!User::where('username', $credentials['username'])->exists()) {
                 $message = 'Invalid username';
             } elseif (!User::where('password', bcrypt($credentials['password']))->exists()) {
                 $message = 'Invalid password';
             }
-        
+
             return view('pages.login')->with('message', $message);
         }
-        
+    }
+
+    public function changePassword(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'password_old' => 'required',
+            'password_new' => 'required|min:4|max:20',
+            'password_confirm' => 'required|same:password_new|min:4|max:20',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->messages()]);
+        } else {
+            $user = User::find(auth()->user()->id);
+            if (Hash::check($request->input('password_old'), $user->password)) {
+                $user->password = Hash::make($request->input('password_confirm'));
+                $user->update();
+                return response()->json('success');
+            } else {
+                return response()->json(['error' => 'Password Incorrect']);
+            }
+        }
     }
 
     public function logout()
